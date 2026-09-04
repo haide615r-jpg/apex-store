1746a5ebcab259273ced1127c447c855146f6022
haide615r-jpg <haide615r@gmail.com>
2026-09-04 18:24:03 +0500

    Add files via upload

--- server.js
+++ server.js
@@ -58,5 +58,5 @@ app.post('/create-checkout-session', async (req, res) => {
     }
 });
 
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Backend Server Running on Port ${PORT}`));
